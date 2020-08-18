3e9a990f5a317328e0b4beaf327ef35c43e50abc
samsog <juwonsogbesan@gmail.com>
2020-08-18 12:38:43 +0100

    Initialise React App Structure

---
+++ webapp/components/Header.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+/**
+ * Main Navigation Component
+ */
+export const Header = () => {
+    return (
+        <header>
+            LLVM Build View
+        </header>
+    )
+}
