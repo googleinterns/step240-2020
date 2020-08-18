7fa418aac7583994056d9d9502efc620e68375b6
MagoDelBlocco <bgdnkln@gmail.com>
2020-08-18 16:05:02 +0000

    add barebones storage classes

---
+++ src/main/java/com/google/sps/storage/StorageWrapper.java
@@ -0,0 +1,30 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.storage;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.sps.storage.StorageWrapperInterface;
+
+/**
+ * TODO(bogdanciobanu): add class explanations
+ */
+public class StorageWrapper implements StorageWrapperInterface<Entity, String> {
+  public void addEntry(final Entity entry) { }
+
+  public Entity getEntry(final String primaryKey) {
+    return null;
+  }
+}
