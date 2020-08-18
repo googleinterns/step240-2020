a2121398e06b827d075e246ccbd46b1bac5bf596
samsog <juwonsogbesan@gmail.com>
2020-08-18 15:47:27 +0100

    Move webapp folder to src/main/

---
+++ src/main/webapp/components/BuildSnapshot.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+/**
+ * Component used to render Build Information.
+ * @param {*} props input properties containg build information
+ */
+export const BuildSnapshot = (props) => {
+    return (
+        <div>
+            Snapshot
+        </div>
+    )
+}
