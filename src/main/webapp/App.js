793c906f9f96e9495e0f52b7be1ad4fe8f4091b0
samsog <juwonsogbesan@gmail.com>
2020-08-19 10:59:25 +0100

    Add space to js-doc

--- src/main/webapp/App.js
+++ src/main/webapp/App.js
@@ -2,6 +2,7 @@ import * as React from "react";
 
 /**
  * Parent Component that is Rendered to the Page.
+ * 
  * @param {*} props arbituary input properties for our application to use
  */
 export const App = (props) => {
