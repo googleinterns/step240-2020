27b721e5ce3d7d62a015baa38258bd9f8b9530d9
samsog <juwonsogbesan@gmail.com>
2020-08-20 12:02:37 +0100

    Create BuildSnapShot Container Component

---
+++ src/main/webapp/components/BuildSnapshotContainer.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+/**
+ * Container Component for BuildSnapshots.
+ * 
+ * @param {*} props input property containing an array of build data to be
+ * rendered through BuildSnapshot.
+ */
+export const BuildSnapshotContainer = (props) => {
+    return (
+        <div>
+            snapshot container.
+        </div>
+    )
+}
