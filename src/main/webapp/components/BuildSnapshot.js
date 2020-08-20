0962f88df74ab912a094bf886671f5fcf9e69b5f
samsog <juwonsogbesan@gmail.com>
2020-08-20 12:10:06 +0100

    Update spelling for comment

--- src/main/webapp/components/BuildSnapshot.js
+++ src/main/webapp/components/BuildSnapshot.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 /**
  * Component used to render Build Information.
- * @param {*} props input properties containg build information
+ * @param {*} props input properties containing build information
  */
 export const BuildSnapshot = (props) => {
     return (
