671c21cb721fc029a0cd5bb7e5974e0a0be058c7
samsog <juwonsogbesan@gmail.com>
2020-08-20 13:27:40 +0100

    Add state variable to track whether BuildSnapshot is opened or closed

--- src/main/webapp/components/BuildSnapshot.js
+++ src/main/webapp/components/BuildSnapshot.js
@@ -5,10 +5,15 @@ import * as React from "react";
  * @param {*} props input properties containing build information
  */
 export const BuildSnapshot = (props) => {
+    // State variable indicating whether the BuildSnapshot is open
+    // isOpen === true indicates the tray should be visible 
+    // and the indicator arrow on the Header should be facing downwards.
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div>
-            <Header data = { }/>
-            <Tray data = { }/>
+            <Header isOpen = {isOpen} data = { }/>
+            <Tray isOpen = {isOpen} data = { }/>
         </div>
     )
 }
