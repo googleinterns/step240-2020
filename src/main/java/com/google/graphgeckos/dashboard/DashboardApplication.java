b9b41509702fbff541972dc022f08fe2e75ebea1
holounic <mia.ilminsckaya@yandex.ru>
2020-08-20 16:15:42 +0300

    fix issues

--- src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
+++ src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
@@ -1,7 +1,7 @@
 package com.google.graphgeckos.dashboard;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class DashboardApplication {
