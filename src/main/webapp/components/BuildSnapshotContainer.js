38eaf6b74602bdb042d4d94352d2e08dc37a3210
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-24 14:32:14 +0000

    Update dummy data to be more realistic

--- src/main/webapp/components/BuildSnapshotContainer.js
+++ src/main/webapp/components/BuildSnapshotContainer.js
@@ -11,12 +11,12 @@ export const BuildSnapshotContainer = (props) => {
     return (
         <Wrapper>
           <BuildSnapshot buildData = {{
-            commitHash: "HASH",
-            description: "Description",
-            repository: "REPO LINK",
+            commitHash: "d8f7965e60e7b975127f7d8bc3dbd670eb93a992",
+            description: "Reduce size of project cards",
+            repository: "https://github.com/googleinterns/step240-2020/",
             status: "PASSED",
             builders: [],
-            timestamp: "Monday"
+            timestamp: "2038-01-19 03:14:07' UTC"
           }}
         />
         </Wrapper>
