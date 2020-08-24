09c1c5031d973b61c58e7870100c1cc4cac58e68
holounic <mia.ilminsckaya@yandex.ru>
2020-08-24 18:43:49 +0300

    change access modifiers

--- src/main/java/com/google/graphgeckos/dashboard/storage/Log.java
+++ src/main/java/com/google/graphgeckos/dashboard/storage/Log.java
@@ -1,4 +1,4 @@
-package com.google.graphgeckos.dashboard.components;
+package com.google.graphgeckos.dashboard.storage;
 
 /**
  * Represents a log of a buildbot.
@@ -9,7 +9,7 @@ public class Log {
   // full log link (e.g "http://lab.llvm.org:8011/builders/mlir-nvidia/builds/6403/logs/stdio")
   private final String link;
 
-  public Log(final String type, final String link) {
+  Log(final String type, final String link) {
     this.type = type;
     this.link = link;
   }
