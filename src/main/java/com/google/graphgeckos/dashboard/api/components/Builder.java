78a98e2363d92d2150a33ddf6e6bacd1f88b1274
holounic <mia.ilminsckaya@yandex.ru>
2020-08-24 17:55:53 +0300

    add full stop at the end of the sentences

--- src/main/java/com/google/graphgeckos/dashboard/api/components/Builder.java
+++ src/main/java/com/google/graphgeckos/dashboard/api/components/Builder.java
@@ -22,21 +22,21 @@ public class Builder {
   }
 
   /**
-   * Returns name of the builder
+   * Returns name of the builder.
    */
   public String getBuilderName() {
     return builderName;
   }
 
   /**
-   * Returns list of logs of the builder
+   * Returns list of logs of the builder.
    */
   public List<Log> getLogs() {
     return logs;
   }
 
   /**
-   * Returns status of the builder (FAILED or PASSED)
+   * Returns status of the builder (FAILED or PASSED).
    */
   public BuilderStatus getStatus() {
     return status;
--- src/main/java/com/google/graphgeckos/dashboard/api/components/Log.java
+++ src/main/java/com/google/graphgeckos/dashboard/api/components/Log.java
@@ -4,9 +4,9 @@ package com.google.graphgeckos.dashboard.api.components;
  * Represents a log of a buildbot.
  */
 public class Log {
-  // log type
+  // log type (e.g "stdio")
   private final String type;
-  // full log link
+  // full log link (e.g "http://lab.llvm.org:8011/builders/mlir-nvidia/builds/6403/logs/stdio")
   private final String link;
 
   public Log(String type, String link) {
@@ -15,14 +15,14 @@ public class Log {
   }
 
   /**
-   * Returns a type of the log
+   * Returns a type of the log.
    */
   public String getType() {
     return type;
   }
 
   /**
-   * Returns a link to the full version of the log
+   * Returns a link to the full version of the log.
    */
   public String getLink() {
     return link;
