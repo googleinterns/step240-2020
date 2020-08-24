80170b87d28669eed66d7fb997c7c7f7a403aef5
holounic <mia.ilminsckaya@yandex.ru>
2020-08-24 17:05:30 +0300

    add javadoc for Log class

--- src/main/java/com/google/graphgeckos/dashboard/api/components/Log.java
+++ src/main/java/com/google/graphgeckos/dashboard/api/components/Log.java
@@ -4,7 +4,9 @@ package com.google.graphgeckos.dashboard.api.components;
  * Represents a log of a buildbot.
  */
 public class Log {
+  // log type
   private final String type;
+  // full log link
   private final String link;
 
   public Log(String type, String link) {
@@ -12,10 +14,16 @@ public class Log {
     this.link = link;
   }
 
+  /**
+   * @return a type of the log
+   */
   public String getType() {
     return type;
   }
 
+  /**
+   * @return a link to the full version of the log
+   */
   public String getLink() {
     return link;
   }
