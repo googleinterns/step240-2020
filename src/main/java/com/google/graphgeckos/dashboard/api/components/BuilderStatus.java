8578a13e7fe9bc1ce0bbf40b3ed6dd4e73bd2cec
holounic <mia.ilminsckaya@yandex.ru>
2020-08-24 16:58:24 +0300

    add enum representation of the builder status

---
+++ src/main/java/com/google/graphgeckos/dashboard/api/components/BuilderStatus.java
@@ -0,0 +1,25 @@
+package com.google.graphgeckos.dashboard.api.components;
+
+
+/**
+ * Represents buildbot statuses: "failed" or "passed"
+ */
+public enum BuilderStatus {
+  FAILED("failed"),
+  PASSED("passed");
+
+  // String representation of the status
+  private final String status;
+
+  BuilderStatus(String status) {
+    this.status = status;
+  }
+
+  /**
+   * @return String representation of the status
+   */
+  public String getStatus() {
+    return status;
+  }
+
+}
