e97e7beca782d5123ce8fcbaec6b54b524c9d930
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 12:14:25 +0000

    Use getField to dynamically set className based on prop

--- src/main/webapp/components/Wrapper.js
+++ src/main/webapp/components/Wrapper.js
@@ -1,8 +1,10 @@
 import * as React from "react";
- 
+import {getField} from "utils/getField";
+
 export const Wrapper = (props) => {
+  const className = getField(props,"className","");
   return (
-    <div>
+    <div className={className}>
       {props.children}
     </div>
   )
