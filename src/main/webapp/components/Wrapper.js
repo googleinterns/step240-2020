a0ff46af474c3684e7f7585720f9149062445c62
samuelsogbesan <47117318+samuelsogbesan@users.noreply.github.com>
2020-08-25 16:36:52 +0100

    Add J-Doc Style Comment

--- src/main/webapp/components/Wrapper.js
+++ src/main/webapp/components/Wrapper.js
@@ -1,6 +1,14 @@
 import * as React from "react";
 import {getField} from "utils/getField";
 
+/**
+  * Container element used to wrap DOM elements.
+  * Used as a abstraction for "div" HTML element.
+  * 
+  * @param props an object specifying the child components
+  * and optionally specifying a className.
+  *
+*/
 export const Wrapper = (props) => {
   const className = getField(props,"className","");
   return (
