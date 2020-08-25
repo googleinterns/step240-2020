3c0bc3865e83d3fd8f439b2ddc645b91473fcfa7
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 10:49:14 +0000

    Use input data to recursively generate BuildSnapshot Components

--- src/main/webapp/components/BuildSnapshotContainer.js
+++ src/main/webapp/components/BuildSnapshotContainer.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Wrapper} from "./Wrapper";
+import {BuildSnapshot} from "./BuildSnapshot";
 
 /**
  * Container Component for BuildSnapshots.
@@ -8,9 +9,10 @@ import {Wrapper} from "./Wrapper";
  * rendered through BuildSnapshot.
  */
 export const BuildSnapshotContainer = (props) => {
+  const data = props.data;
   return (
     <Wrapper>
-    snapshot container.
+    {data.map(snapshotData => <BuildSnapshot data={snapshotData}/>)}
     </Wrapper>
   );
 }
