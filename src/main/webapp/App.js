075d06ec62a91543e3d7caa86656e5ea9e50b17d
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 15:54:07 +0000

    Add full stop to comment.

--- src/main/webapp/App.js
+++ src/main/webapp/App.js
@@ -6,7 +6,7 @@ import {BuildSnapshotContainer} from "./components/BuildSnapshotContainer";
 /**
  * Main App that is Rendered to the Page.
  * 
- * @param {*} props arbituary input properties for our application to use
+ * @param {*} props arbituary input properties for our application to use.
  */
 export const App = (props) => {
   return (
