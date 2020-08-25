28a90ebe18668b15d59252c890462bfe6f4d3242
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 12:31:47 +0000

    Add Jdoc style comment

--- src/main/webapp/components/utils/getFields.js
+++ src/main/webapp/components/utils/getFields.js
@@ -1,3 +1,11 @@
+/**
+  * Creates a new object whose fields are either equal to obj[field] or defaultValue.
+  * Useful for extracting a subset of fields from an object.
+  *
+  * @param obj the object to query the fields for.
+  * @param fields an array of the target fields, in string form. 
+  * @param defaultValue 
+ */
 const getFields = (obj, fields, defaultValue) => {
   const result = { };
   for(const field of fields) result[field] = getField(obj, field, defaultValue);
