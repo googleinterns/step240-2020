eafe4e99304e8160ca373cb937997e3665d34090
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 11:54:31 +0000

    Add helpers to fetch object fields, Use helper to get header and tray data

--- src/main/webapp/components/BuildSnapshot.js
+++ src/main/webapp/components/BuildSnapshot.js
@@ -6,9 +6,23 @@ import * as React from "react";
  * @param {*} props input properties containing build information
  */
 export const BuildSnapshot = (props) => {
+  const headerFields = ["description", "commitHash", "repository", "status"];
+  const trayFields = ["builders", "timestamp"];
+
+  const headerData = getFields(props.buildData, headerFields, "");
+  const trayData = getFields(props.buildData, trayFields, "");
+
   return (
     <div>
     Snapshot
     </div>
   );
 }
+
+const getFields = (obj, fields, defaultValue) => {
+  const result = { };
+  for(const field of fields) result[field] = getField(obj, field, defaultValue);
+  return result;
+}
+
+const getField = (obj, field, defaultValue) => obj[field] !== undefined ? obj[field] : (defaultValue !== undefined ? defaultValue : null);
