bf39941ab315db60d0c6e7db5d4b24d00ac62d7f
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-25 13:44:01 +0000

    Implement state variable to indicate if element 'open'

--- src/main/webapp/components/BuildSnapshot.js
+++ src/main/webapp/components/BuildSnapshot.js
@@ -7,6 +7,11 @@ import {getFields} from "./utils/getFields.js";
  * @param {*} props input properties containing build information
  */
 export const BuildSnapshot = (props) => {
+  // State variable indicating whether the BuildSnapshot is open
+  // isOpen === true indicates the tray should be visible 
+  // and the indicator arrow on the Header should be facing downwards.
+  const [isOpen, setIsOpen] = React.useState(false);
+  
   const headerFields = ["description", "commitHash", "repository", "status"];
   const trayFields = ["builders", "timestamp"];
 
