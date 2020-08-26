f2f0296aa1ef5e21866fe0acb0e9336d66b2bd8c
holounic <mia.ilminsckaya@yandex.ru>
2020-08-26 18:38:33 +0300

    create POJO for commit data

--- src/main/java/com/google/graphgeckos/dashboard/fetchers/CommitData.java
+++ src/main/java/com/google/graphgeckos/dashboard/fetchers/CommitData.java
@@ -1,16 +1,28 @@
 package com.google.graphgeckos.dashboard.fetchers;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommitData {
+  
+  @JsonProperty("after")
   private final String commitHash;
-  private final String timestamp;
+
+  private String timestamp;
+
+  @JsonProperty("ref")
   private final String branch;
 
-  CommitData(String commitHash, String timestamp, String branch) {
+  @JsonProperty("repository")
+  private void unpackTimestamp(Map<String, Object> repository) {
+    timestamp = repository.get("updated-at").toString();
+  }
+
+  CommitData(String commitHash, String branch) {
     this.commitHash = commitHash;
-    this.timestamp = timestamp;
     this.branch = branch;
   }
 
@@ -24,5 +36,10 @@ public class CommitData {
 
   public String getTimestamp() {
     return timestamp;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("branch: %s, comitHash: %s, timestamp, %s", branch, commitHash, timestamp);
   }
 }
