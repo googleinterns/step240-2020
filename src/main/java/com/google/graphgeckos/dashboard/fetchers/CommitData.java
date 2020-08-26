1c25e630b80c3915e72d50c884c8f3d04cf00218
holounic <mia.ilminsckaya@yandex.ru>
2020-08-26 20:05:53 +0300

    add javadocs

--- src/main/java/com/google/graphgeckos/dashboard/fetchers/CommitData.java
+++ src/main/java/com/google/graphgeckos/dashboard/fetchers/CommitData.java
@@ -2,26 +2,52 @@ package com.google.graphgeckos.dashboard.fetchers;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommitData {
 
-  // Git commit hash
-  @JsonProperty("after")
-  private final String commitHash;
+  // name of the branch
+  private String branch;
 
+  /**
+   * Extracts the name of the branch from the reference.
+   * @param ref Git reference
+   */
   @JsonProperty("ref")
-  private final String branch;
+  private void extractBranch(String ref) {
+    String[] refComponents = ref.split("/");
+    branch = refComponents[refComponents.length - 1];
+  }
 
+  // Git commit hash
+  private String commitHash;
+
+  // time of the Git commit push
   private String timestamp;
 
+  /**
+   * Extracts commit id (also known as commit hash) and timestamp nested fields from the data of the head_commit
+   * field.
+   * Assigns the id value to {@code commitHash} and the timestamp value to {@code timestamp}.
+   * @param headCommit representation of the head_commit field of the GitHub API request json
+   */
+  @JsonProperty("head_commit")
+  private void unpackHeadCommit(Map<String, Object> headCommit) {
+    commitHash = headCommit.get("id").toString();
+    timestamp = headCommit.get("timestamp").toString();
+  }
+
+  // link to to the repository
   private String repositoryLink;
 
+  /**
+   * Extracts link to the working repository (html_url field value) from the data of the repository field.
+   * Assigns the extracted value to {@code repositoryLink}.
+   * @param repository representation of the repository field of the GitHub API request json
+   */
   @JsonProperty("repository")
-  private void unpackRepositoryData(Map<String, Object> repository) {
-    timestamp = repository.get("updated-at").toString();
+  private void extractRepositoryLink(Map<String, Object> repository) {
     repositoryLink = repository.get("html_url").toString();
   }
 
@@ -30,19 +56,32 @@ public class CommitData {
     this.branch = branch;
   }
 
+  /**
+   * Returns name of the Git branch where the tested changes were made.
+   */
   public String getBranch() {
     return branch;
   }
 
+  /**
+   * Returns Git commit hash of the last commit.
+   */
   public String getCommitHash() {
     return commitHash;
   }
 
+  /**
+   * Returns time of when the commit was pushed.
+   */
   public String getTimestamp() {
     return timestamp;
   }
 
-  public String getRepositoryName() {
-    return repositoryName;
+  /**
+   * Returns link to the Git repository where the last commit was made.
+   */
+  public String getRepositoryLink() {
+    return repositoryLink;
   }
+
 }
