8fcc9dbab0967730e342369c97581215e3116469
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-27 14:04:00 +0000

    Create initial Component Structure

---
+++ src/main/webapp/components/Navigation.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+/**
+ * Main Navigation List.
+ * Instance will be embedded in ./Header component.
+ */
+export const Navigation = () => {
+  return (
+    <ul>
+
+    </ul>
+  );
+}
