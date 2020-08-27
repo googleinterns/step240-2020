2119e5a681c061e1815744cb3e340294321a6066
holounic <mia.ilminsckaya@yandex.ru>
2020-08-27 14:33:00 +0300

    implement github api fetcher

--- src/main/java/com/google/graphgeckos/dashboard/fetchers/GithubFetcher.java
+++ src/main/java/com/google/graphgeckos/dashboard/fetchers/GithubFetcher.java
@@ -1,6 +1,23 @@
 package com.google.graphgeckos.dashboard.fetchers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GithubFetcher {
 
-
+  /**
+   * Handles POST requests from the GitHub API. Setting GitHub Webhooks is required.
+   * Extracts required data from the json {@see CommitData} and adds it to the Storage.
+   *
+   * Class that provide access to the Storage is required to implement the functionality.
+   * @param commitData json from the GitHub API
+   */
+  @PostMapping("/github-info")
+  public void postGithubInfo(@RequestBody CommitData commitData) {
+    System.out.println(commitData);
+    // TODO: implement described functionality. (Couldn't be implemented now since there is no Class to access storage).
+    assert false;
+  }
 }
