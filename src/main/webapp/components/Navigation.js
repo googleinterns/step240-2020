f8c4b0d7f78a215865a7ab00e27634dc646bf148
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-27 14:11:26 +0000

    Add NavigationItem reusable component

--- src/main/webapp/components/Navigation.js
+++ src/main/webapp/components/Navigation.js
@@ -1,13 +1,17 @@
 import * as React from "react";
 
 /**
+ */
+const NavigationItem = props => <li><a href={props.href}>{props.innerText}</a></li>
+
+/**
  * Main Navigation List.
  * Instance will be embedded in ./Header component.
  */
-export const Navigation = () => {
+export const Navigation = (props) => {
   return (
     <ul>
-
+      {props.options.map(option => <NavigationItem href={option.link} innerText={option.text}/>)}
     </ul>
   );
 }
