e34d444e60f79ec2224d03142eb1cd7a1098c97f
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-28 11:53:38 +0000

    Used clang-format to format getField

--- src/main/webapp/components/utils/getField.js
+++ src/main/webapp/components/utils/getField.js
@@ -1,12 +1,13 @@
 /**
-  * Useful for extracting a field from an object, with a fallback (defaultValue).
-  * Useful where "undefined", is not a desired result for a missing field.
-  *
-  * @param obj the object to query the fields for.
-  * @param field the target field, in string form. 
-  * @param defaultValue the value to be used if obj[field] === undefined.
+ * Useful for extracting a field from an object, with a fallback (defaultValue).
+ * Useful where "undefined", is not a desired result for a missing field.
+ *
+ * @param obj the object to query the fields for.
+ * @param field the target field, in string form.
+ * @param defaultValue the value to be used if obj[field] === undefined.
  */
 export const getField = (obj, field, defaultValue) => {
   return obj[field] !== undefined ?
-  obj[field] : (defaultValue !== undefined ? defaultValue : null);
+      obj[field] :
+      (defaultValue !== undefined ? defaultValue : null);
 }
