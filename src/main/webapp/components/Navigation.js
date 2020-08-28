a0335bc5a3fc5aa671d04f9dbe32c349f95d154d
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-28 13:02:18 +0000

    Format with clang-formatter

--- src/main/webapp/components/Navigation.js
+++ src/main/webapp/components/Navigation.js
@@ -1,18 +1,20 @@
-import * as React from "react";
+import * as React from 'react';
 
 /**
-  * Main List Element to be displayed on the Navigation.
-  * @param {href: a URL String to a resource, innerText: The display text}
+ * Main List Element to be displayed on the Navigation.
+ *
+ * @param {href: a URL String to a resource, innerText: The display text}
  */
-const NavigationItem = props => <li><a href={props.href}>{props.innerText}</a></li>
-
-/**
- * Main Navigation List.
- * Instance will be embedded in ./Header component.
- * @param {options: [option]} props arbituary object containing an array of
- * options, where each option = {link:String, text: String}.
- */
-export const Navigation = (props) => {
+const NavigationItem = props =>
+    <li><a href = {props.href}>{props.innerText}</a></li>
+    /**
+     * Main Navigation List.
+     * Instance will be embedded in ./Header component.
+     *
+     * @param {options: [option]} props arbituary object containing an array of
+     * options, where each option = {link:String, text: String}.
+     */
+    export const Navigation = (props) => {
   return (
     <ul>
       {props.options.map(option => <NavigationItem href={option.link} innerText={option.text}/>)}
