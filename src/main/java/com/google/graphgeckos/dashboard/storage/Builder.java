4a0c0b7b292585828d611e0e65ec3c3c3263540a
MagoDelBlocco <bgdnkln@gmail.com>
2020-08-28 15:32:58 +0000

    reworked documentation

---
+++ src/main/java/com/google/graphgeckos/dashboard/storage/Builder.java
@@ -0,0 +1,73 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.graphgeckos.dashboard.storage;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
+
+/**
+ * Represents the information about a single buildbot.
+ */
+@Entity(name = "builder")
+public class Builder {
+
+  private final String builderName;
+
+  // The logs of each compilation stage, stored as described by {@link #Log Log}.
+  private final List<Log> logs;
+
+  // Builder compilation status, as described by {@link #BuilderStatus BuilderStatus}.
+  private final BuilderStatus status;
+
+  /**
+   * Converts a {@link #ParsedBuildbotData ParsedBuildbotData} object to a Builder object.
+   * The fields should be sanitized before this operation, as no field in Builder may be
+   * null.
+   */
+  Builder(ParsedBuildBotData botData) {
+    this.builderName = botData.getBuilderName();
+    this.logs = new ArrayList<>(botData.getLogs());
+    this.status = botData.getStatus();
+  }
+
+  /**
+   * Returns the name of the builder bot.
+   *
+   * @return builderName. Cannot be null.
+   */
+  public String getBuilderName() {
+    return builderName;
+  }
+
+  /**
+   * Returns the list of logs for each compilation stage.
+   *
+   * @return the list of logs. Cannot be null.
+   */
+  public List<Log> getLogs() {
+    return logs;
+  }
+
+  /**
+   * Returns the compilation status of the builder.
+   *
+   * @return compile status. Cannot be null.
+   */
+  public BuilderStatus getStatus() {
+    return status;
+  }
+
+}
