f72ea51639f5f27d2f8b4d98839c36015a98449d
holounic <mia.ilminsckaya@yandex.ru>
2020-08-30 19:49:48 +0300

    add newline

--- src/test/java/com/google/graphgeckos/dashboard/GitHubControllerTests.java
+++ src/test/java/com/google/graphgeckos/dashboard/GitHubControllerTests.java
@@ -51,5 +51,5 @@ public class GitHubControllerTests {
       .andExpect(jsonPath("timestamp").value(REAL_JSON.getTimestamp()))
       .andExpect(jsonPath("repositoryLink").value(REAL_JSON.getRepositoryLink()));
   }
-  
+
 }
