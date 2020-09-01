eeca55ee411014bb401718a0609c78bfd04c4f40
samuelsogbesan <47117318+samuelsogbesan@users.noreply.github.com>
2020-09-01 09:00:51 +0100

    Update Spacing

--- src/main/webapp/components/Navigation.js
+++ src/main/webapp/components/Navigation.js
@@ -7,17 +7,17 @@ import * as React from 'react';
  */
 const NavigationItem = props =>
     <li><a href = {props.href}>{props.innerText}</a></li>
-    /**
-     * Main Navigation List.
-     * Instance will be embedded in ./Header component.
-     *
-     * @param {options: [option]} props arbituary object containing an array of
-     * options, where each option = {link:String, text: String}.
-     */
-    export const Navigation = (props) => {
+/**
+ * Main Navigation List.
+ * Instance will be embedded in ./Header component.
+ *
+ * @param {options: [option]} props arbituary object containing an array of
+ * options, where each option = {link:String, text: String}.
+ */
+export const Navigation = (props) => {
   return (
     <ul>
-      {props.options.map(option => <NavigationItem href={option.link} innerText={option.text}/>)}
+      {props.options.map (option => <NavigationItem href={option.link} innerText={option.text}/>)}
     </ul>
   );
 }
