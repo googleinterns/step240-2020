bbcccfe32aec60a1ce0ba9d3e4d864b05890be79
MagoDelBlocco <bgdnkln@gmail.com>
2020-09-01 16:48:12 +0000

    fix imports

--- src/test/java/com/google/graphgeckos/dashboard/storage/DatastoreRepositoryTests.java
+++ src/test/java/com/google/graphgeckos/dashboard/storage/DatastoreRepositoryTests.java
@@ -14,7 +14,8 @@
 
 package com.google.graphgeckos.dashboard.storage;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
