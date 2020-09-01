33e3d895c45f496d2760f724e997bcb19a31ee97
holounic <mia.ilminsckaya@yandex.ru>
2020-09-01 10:53:29 +0300

    add helpful comments for the future work

--- src/main/java/com/google/graphgeckos/dashboard/api/BuilderController.java
+++ src/main/java/com/google/graphgeckos/dashboard/api/BuilderController.java
@@ -2,17 +2,20 @@ package com.google.graphgeckos.dashboard.api;
 
 import com.google.graphgeckos.dashboard.storage.BuildInfo;
 import com.google.graphgeckos.dashboard.storage.DataRepository;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BuilderController {
 
+  // Bean with @ApplicationScope? 
   private static DataRepository dataRepository;
 
-  // TODO: mashasamsikova implement it
-  @GetMapping("/builders/number={number}/offset={offset}")
+  // TODO: [issue #69] implement interaction between REST API and Storage
+  @RequestMapping(value = "/builders/number={number}/offset={offset}",
+                  method = RequestMethod.GET, headers = {"content-type=application/json"})
   public Iterable<BuildInfo> getBuildInfo(@PathVariable int number, @PathVariable int offset) {
     return dataRepository.getLastRevisionEntries(number, offset);
   }
