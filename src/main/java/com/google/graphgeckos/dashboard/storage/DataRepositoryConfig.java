76255ef2c77b1218ea8b30fdbba381c94449b3bb
holounic <mia.ilminsckaya@yandex.ru>
2020-09-01 11:18:40 +0300

    add ApplicationScope annotation

--- src/main/java/com/google/graphgeckos/dashboard/storage/DataRepositoryConfig.java
+++ src/main/java/com/google/graphgeckos/dashboard/storage/DataRepositoryConfig.java
@@ -2,11 +2,13 @@ package com.google.graphgeckos.dashboard.storage;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 @Configuration
 public class DataRepositoryConfig {
 
   @Bean("dataRepository")
+  @ApplicationScope
   public DataRepository getDataRepository() {
     // TODO: return new DataRepositoryImplementation();
     return null;
