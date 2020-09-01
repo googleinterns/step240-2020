75865b66637492bec8dc9c6f64cc4e4219056f1e
holounic <mia.ilminsckaya@yandex.ru>
2020-09-01 11:17:47 +0300

    change function name in DataRepository config and bean name to reflect the purpose of them

--- src/main/java/com/google/graphgeckos/dashboard/storage/DataRepositoryConfig.java
+++ src/main/java/com/google/graphgeckos/dashboard/storage/DataRepositoryConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataRepositoryConfig {
 
-  @Bean("dataRepo")
-  public DataRepository getRepository() {
+  @Bean("dataRepository")
+  public DataRepository getDataRepository() {
     // TODO: return new DataRepositoryImplementation();
     return null;
   }
