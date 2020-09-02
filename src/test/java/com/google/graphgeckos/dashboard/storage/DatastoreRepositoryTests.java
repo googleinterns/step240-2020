e921682aa9d1353b67c2c9acf7d611e785d45a32
MagoDelBlocco <bgdnkln@gmail.com>
2020-09-02 13:44:01 +0000

    add parallel tests

--- src/test/java/com/google/graphgeckos/dashboard/storage/DatastoreRepositoryTests.java
+++ src/test/java/com/google/graphgeckos/dashboard/storage/DatastoreRepositoryTests.java
@@ -20,8 +20,7 @@ import static org.junit.Assert.assertTrue;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.cloud.Timestamp;
-import java.util.Calendar;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -49,23 +48,26 @@ public class DatastoreRepositoryTests {
   }
 
   @Test
-  public void testValidRequestSequenceValidData() {
+  public void testRequestSequenceValidData() {
     DatastoreRepository storage = new DatastoreRepository();
     
+    // Test valid addition
     Assert.assertTrue(storage.createRevisionEntry(getDummyEntity("1")));
     Assert.assertEquals(getDummyEntity("1"), storage.getRevisionEntry("1"));
-    Assert.assertTrue(storage.updateRevisionEntry(getDummyUpdate("1")));
 
+    // Test valid update
+    Assert.assertTrue(storage.updateRevisionEntry(getDummyUpdate("1")));
     BuildInfo dummy = getDummyEntity("1");
     dummy.addBuilder(new Builder(getDummyUpdate("1")));
     Assert.assertEquals(dummy, storage.getRevisionEntry("1"));
 
+    // Test valid deletion
     Assert.assertTrue(storage.deleteRevisionEntry("1"));
     Assert.assertEquals(null, storage.getRevisionEntry("1"));
   }
 
   @Test
-  public void testValidRequestSequenceNullData() {
+  public void testRequestsNullData() {
     DatastoreRepository storage = new DatastoreRepository();
 
     Assert.assertFalse(storage.createRevisionEntry(null));
@@ -75,30 +77,47 @@ public class DatastoreRepositoryTests {
   }
 
   @Test
-  public void testValidRequestSequenceInvalidData() {
-    DatastoreRepository storage = new DatastoreRepository(initialCleanupDelayDefault,
-                                                          cleanupPeriodDefault,
-                                                          cleanupPeriodTimeunitDefault,
-                                                          ttlDefault,
-                                                          ttlTimeUnitDefault);
+  public void testRequestSequenceInvalidData() {
+    DatastoreRepository storage = new DatastoreRepository();
+
+    // Test adding same entry
     Assert.assertTrue(storage.createRevisionEntry(getDummyEntity("1")));
     Assert.assertFalse(storage.createRevisionEntry(getDummyEntity("1")));
 
+    // Test updating inexistent entry
     Assert.assertFalse(storage.updateRevisionEntry(getDummyUpdate("2")));
 
+    // Test getting inexistent entry
     Assert.assertEquals(null, storage.getRevisionEntry("2"));
 
+    // Test deleting inexistent entry
     Assert.assertTrue(storage.deleteRevisionEntry("2"));
   }
 
   @Test
-  public void testInvalidRequestSequenceValidData() {
+  public void testParallelBurstRequests() {
+    DatastoreRepository storage = new DatastoreRepository();
 
-  }
+    Assert.assertTrue(storage.createRevisionEntry(getDummyEntity("1")));
+    Assert.assertTrue(storage.createRevisionEntry(getDummyEntity("2")));
 
-  @Test
-  public void testParallelValidRequests() {
+    Runnable burstRequest = () -> {
+      for (int i = 0; i < 10; ++i) {
+        Assert.assertTrue(storage.updateRevisionEntry(getDummyUpdate("1")));
+        Assert.assertTrue(storage.updateRevisionEntry(getDummyUpdate("2")));
+      }
+    }
 
+    Thread thread1 = new Thread(burstRequest);
+    Thread thread2 = new Thread(burstRequest);
+    Thread thread3 = new Thread(burstRequest);
+
+    thread1.start();
+    thread2.start();
+    thread3.start();
+
+    Assert.assertTrue(storage.deleteRevisionEntry("1"));
+    Assert.assertTrue(storage.deleteRevisionEntry("2"));
   }
 
   @Test
