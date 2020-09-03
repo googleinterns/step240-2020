f22f1300fef3acf1208f27f790b5d4712ab97b6d
holounic <mia.ilminsckaya@yandex.ru>
2020-09-03 21:13:19 +0300

    remove empty line

--- src/main/java/com/google/graphgeckos/dashboard/storage/Builder.java
+++ src/main/java/com/google/graphgeckos/dashboard/storage/Builder.java
@@ -17,7 +17,6 @@ package com.google.graphgeckos.dashboard.storage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
