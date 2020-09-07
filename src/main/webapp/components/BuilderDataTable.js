f10c59a0440e7e5b57f0165f7740d9ff1b7be12c
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-07 13:24:10 +0000

    Wrap lines

--- src/main/webapp/components/BuilderDataTable.js
+++ src/main/webapp/components/BuilderDataTable.js
@@ -11,8 +11,10 @@ const HEADERS = ['no.', 'text', 'log'];
  * Table responsible for displaying the data for a builder.
  *
  * @param {Object[]} props.buildSteps - The build steps for a given builder.
- * @param {string} props.buildSteps[].step_number - The relative order of the build step.
- * @param {string} props.buildSteps[].text - Output text related to the build step.
+ * @param {string} props.buildSteps[].step_number - The relative order of 
+ * the build step.
+ * @param {string} props.buildSteps[].text - Output text related to 
+ * the build step.
  * @param {string} props.buildSteps[].logs - A URL pointing to the log file.
  */
 export const BuilderDataTable = props => {
