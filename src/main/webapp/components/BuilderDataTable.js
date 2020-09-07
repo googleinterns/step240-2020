50dbadc0ce7d0173780b3e8b2013910deec073b1
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-07 12:57:28 +0000

    Update comments and rename props.data to props.buildStep

--- src/main/webapp/components/BuilderDataTable.js
+++ src/main/webapp/components/BuilderDataTable.js
@@ -2,6 +2,14 @@ import * as React from 'react';
 
 const HEADERS = ['no', 'text', 'log'];
 
+/**
+ * Table responsible for storing the data for a builder.
+ *
+ * @param {Object[]} props.buildSteps - The build steps for a given builder.
+ * @param {string} props.buildSteps[].step_number - The relative order of the build step.
+ * @param {string} props.buildSteps[].text - Output text related to the build step.
+ * @param {string} props.buildSteps[].log - A URL pointing to the log file. 
+ */
 export const BuilderDataTable = props => {
   return (
     <table className='data-table'>
@@ -9,7 +17,7 @@ export const BuilderDataTable = props => {
         <tr>{HEADERS.map(header => <th>{header}</th>)}</tr>
       </thead>
       <tbody>
-        {props.data.map(datapoint => 
+        {props.buildSteps.map(datapoint => 
           <tr>
             {HEADERS.forEach(header => <td>{datapoint[header]}</td>)}
           </tr>
