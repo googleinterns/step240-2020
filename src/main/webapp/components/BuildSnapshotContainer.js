48edcfe876011159fc9154e36db0fb1a6578ef5d
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-07 09:12:16 +0000

    Add spaces before *

--- src/main/webapp/components/BuildSnapshotContainer.js
+++ src/main/webapp/components/BuildSnapshotContainer.js
@@ -13,11 +13,11 @@ export const BuildSnapshotContainer = React.memo((props) => {
     const [data, setData] = React.useState([]);
 
     /**
-    * Fetch data when component is mounted.
-    * Pass in empty array as second paramater to prevent
-    * infinite callbacks as component refreshes.
-    * @see <a href="www.robinwieruch.de/react-hooks-fetch-data">Fetching</a>
-    */
+     * Fetch data when component is mounted.
+     * Pass in empty array as second paramater to prevent
+     * infinite callbacks as component refreshes.
+     * @see <a href="www.robinwieruch.de/react-hooks-fetch-data">Fetching</a>
+     */
     React.useEffect(() => {
       fetch(SOURCE).then(res => setData(res.json()));
     }, []);
