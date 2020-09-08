bc6a4f151987b7b3cf8b4ce41031f4a2c879f8b7
MagoDelBlocco <bgdnkln@gmail.com>
2020-09-08 16:28:55 +0000

    update buildbotdata

--- src/main/java/com/google/graphgeckos/dashboard/datatypes/BuildBotData.java
+++ src/main/java/com/google/graphgeckos/dashboard/datatypes/BuildBotData.java
@@ -18,6 +18,8 @@ import com.google.cloud.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
+import org.springframework.cloud.gcp.data.datastore.core.mapping.Field;
+import org.springframework.data.annotation.Transient;
 
 /**
  * Contains the information retrieved from a single build bot. It is used as a member
@@ -33,23 +35,44 @@ public class BuildBotData {
    * it in the right {@link BuildInfo} in the storage.
    * Used in {@link com.google.graphgeckos.dashboard.storage.DatastoreRepository}.
    */
+  @Transient
+  @Field(name = "commitHash")
   private String commitHash;
 
-  /* The timestamp of the build. */
+  /** 
+   * The timestamp of the build. 
+   */
+  @Field(name = "timestamp")
   private Timestamp timestamp;
 
-  /* Name of the buildbot. */
+  /**
+   * Name of the buildbot.
+   */
+  @Field(name = "name")
   private String name;
 
   /**
    * The logs of each compilation stage, stored as described by {@link Log}.
    */
+  @Field(name = "logs")
   private final List<Log> logs = new ArrayList<>();
 
   /**
    * Builder compilation status, as described by {@link BuilderStatus}.
    */
+  @Field(name = "status")
   private BuilderStatus status;
+
+  /**
+   * Sets all fields to null. Only to be used by Spring GCP.
+   */
+  public BuildBotData() {
+    this.commitHash = null;
+    this.timestamp = null;
+    this.name = null;
+    this.logs = null;
+    this.status = null;
+  }
 
   public BuildBotData(String commitHash, String name, List<Log> logs, BuilderStatus status) {
     this.commitHash = commitHash;
@@ -57,8 +80,6 @@ public class BuildBotData {
     this.logs.addAll(logs);
     this.status = status;
   }
-
-  public BuildBotData() {}
 
   /**
    * Returns the name of the builder bot. Cannot be null.
@@ -93,6 +114,41 @@ public class BuildBotData {
    */
   public Timestamp getTimestamp() {
     return timestamp;
+  }
+
+    /**
+   * Returns the name of the builder bot. Cannot be null.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Returns the list of logs for each compilation stage. Cannot be null.
+   */
+  public void getLogs(List<Log> logs) {
+    this.logs = new ArrayList<>(logs);
+  }
+
+  /**
+   * Returns the compilation status of the builder. Cannot be null.
+   */
+  public void getStatus(BuilderStatus status) {
+    this.status = status;
+  }
+
+  /**
+   * Returns the commit hash of the commit tested by the current buildbot.
+   */
+  public void getCommitHash(String commitHash) {
+    this.commitHash = commitHash;
+  }
+
+  /**
+   * Returns the timestamp of the build.
+   */
+  public void getTimestamp(Timestamp timestamp) {
+    this.timestamp = timestamp;
   }
 
 }
