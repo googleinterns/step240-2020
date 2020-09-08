09323a7bd188c348be1bd0fdc1b1d36acb5d82bc
holounic <mia.ilminsckaya@yandex.ru>
2020-09-09 01:05:54 +0300

    add comments for tests

--- src/test/java/com/google/graphgeckos/dashboard/api/DashboardControllerTest.java
+++ src/test/java/com/google/graphgeckos/dashboard/api/DashboardControllerTest.java
@@ -35,17 +35,19 @@ public class DashboardControllerTest {
   private MockMvc mvc;
 
   private final String COMMIT_HASH = "1234";
-
   private final Timestamp TIMESTAMP = Timestamp.now();
-
   private final String BRANCH = "branch a";
+  private final GitHubData INITIAL_GITHUB_DATA = new GitHubData(COMMIT_HASH, TIMESTAMP, BRANCH);
 
+  /* Builders names. */
   private final String NAME_A = "Builder A";
   private final String NAME_B = "Builder B";
 
+  /* Builders statuses. */
   private final String STATUS_A = "PASSED";
   private final String STATUS_B = "FAILED";
 
+  /* Builders logs components. */
   private final String LOG_TYPE_1 = "type 1";
   private final String LOG_TYPE_2 = "type 2";
   private final String LOG_TYPE_3 = "type 3";
@@ -53,20 +55,23 @@ public class DashboardControllerTest {
   private final String LOG_LINK_2 = "link 2";
   private final String LOG_LINK_3 = "link 3";
 
+  /* Builders logs. */
   private final List<Log> LOGS_A = Arrays.asList(new Log(LOG_TYPE_1, LOG_LINK_1),
                                                new Log(LOG_TYPE_2, LOG_LINK_2));
   private final List<Log> LOGS_B = Collections.singletonList(new Log(LOG_TYPE_3, LOG_LINK_3));
-
-  private final GitHubData INITIAL_GITHUB_DATA = new GitHubData(COMMIT_HASH, TIMESTAMP, BRANCH);
-
-  private final BuildInfo BUILD_INFO = new BuildInfo(INITIAL_GITHUB_DATA);
 
   private final BuildBotData BUILDER_A = new BuildBotData(
                                         COMMIT_HASH, NAME_A, LOGS_A, BuilderStatus.valueOf(STATUS_A));
   private final BuildBotData BUILDER_B = new BuildBotData(
                                          COMMIT_HASH, NAME_B, LOGS_B, BuilderStatus.valueOf(STATUS_B));
 
+  private final BuildInfo BUILD_INFO = new BuildInfo(INITIAL_GITHUB_DATA);
+
+
+  /* Illegal revision value. Revision must be >= 0. */
   private final int MINUS_ONE_REVISION = -1;
+
+  /* Valid revision and offset values. */
   private final int ZERO_REVISIONS = 0;
   private final int ONE_REVISION = 1;
   private final int OFFSET_ZERO = 0;
@@ -83,8 +88,12 @@ public class DashboardControllerTest {
     BUILD_INFO.addBuilder(BUILDER_B);
   }
 
+  /**
+   * Given: number of the revision to return = 0, any offset >= 0
+   * Expected behaviour: empty JSON list, Response Status 200 (OK).
+   */
   @Test
-  public void whenGivenZeroRevisionsOffsetZeroReturnsEmptyJson() throws Exception {
+  public void whenGivenZeroRevisionsOffsetZeroReturnsEmptyJSONArray() throws Exception {
 
     given(datastoreRepository.getLastRevisionEntries(ZERO_REVISIONS, OFFSET_ZERO))
       .willReturn(Collections.emptyList());
@@ -100,6 +109,17 @@ public class DashboardControllerTest {
           .andExpect(content().string(emptyJsonList));
   }
 
+  /**
+   * Should return the JSON list of length.
+   *
+   * Given: number of entries to return >= 0; offset b, b >= 0.
+   * Expected behaviour: JSON list of length = number
+   * of entries to return, more about offset and number at
+   * {@link com.google.graphgeckos.dashboard.storage.DataRepository::getLastRevision}.
+   * Returned list contains JSON representations of the BuildBotInfo instances.
+   * Information about this JSON representations could be found at
+   * {@link com.google.graphgeckos.dashboard.datatypes.BuildInfo}.
+   */
   @Test
   public void whenGivenOneRevisionOffsetZeroReturnsOneBuildInfoJson() throws Exception {
 
@@ -121,8 +141,17 @@ public class DashboardControllerTest {
           .andExpect(jsonPath("$[0].builders[0].logs[0].link").value(LOG_LINK_1));
   }
 
+  /**
+   * Number and offset should be greater or equal to zero, otherwise the IllegalArgumentException is
+   * thrown inside the tested request method. If the described situation occurs, the request method
+   * should set the response status to "Bad request".
+   *
+   * Given: illegal argument value/values.
+   * Expected behaviour: response status 400(Bad request).
+   */
   @Test
   public void setsRequestTypeToBadRequestWhenIllegalArgumentExceptionIsThrownByDatastoreRepository() throws Exception {
+
     given(datastoreRepository.getLastRevisionEntries(MINUS_ONE_REVISION, OFFSET_ZERO))
       .willThrow(new IllegalArgumentException());
 
