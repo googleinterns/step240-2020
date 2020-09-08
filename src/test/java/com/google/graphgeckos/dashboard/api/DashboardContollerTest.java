49f92238cba352c0e11a1227c69157faa6e32766
holounic <mia.ilminsckaya@yandex.ru>
2020-09-08 15:00:10 +0300

    add checker

--- src/test/java/com/google/graphgeckos/dashboard/api/DashboardContollerTest.java
+++ src/test/java/com/google/graphgeckos/dashboard/api/DashboardContollerTest.java
@@ -18,7 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -34,27 +33,27 @@ public class DashboardContollerTest {
   @Autowired
   private MockMvc mvc;
 
-  private final String COMMIT_HASH_A = "1234";
-  private final String COMMIT_HASH_B = "abcd";
+  private static final String COMMIT_HASH_A = "1234";
+  private static final String COMMIT_HASH_B = "abcd";
 
-  private final Timestamp TIMESTAMP = Timestamp.now();
+  private static final Timestamp TIMESTAMP = Timestamp.now();
 
-  private final String BRANCH_A = "branch a";
-  private final String BRANCH_B = "branch b";
+  private static final String BRANCH_A = "branch a";
+  private static final String BRANCH_B = "branch b";
 
-  private final List<Log> LOGS_A = Arrays.asList(new Log("type 1", "link1"),
+  private static final List<Log> LOGS_A = Arrays.asList(new Log("type 1", "link1"),
                                                new Log("type 2", "link2"));
-  private final List<Log> LOGS_B = Collections.singletonList(new Log("type 3", "link3"));
+  private static final List<Log> LOGS_B = Collections.singletonList(new Log("type 3", "link3"));
 
-  private final GitHubData INITIAL_GITHUB_DATA_A = new GitHubData(COMMIT_HASH_A, TIMESTAMP, BRANCH_A);
-  private final GitHubData INITIAL_GITHUB_DATA_B = new GitHubData(COMMIT_HASH_B, TIMESTAMP, BRANCH_B);
+  private static final GitHubData INITIAL_GITHUB_DATA_A = new GitHubData(COMMIT_HASH_A, TIMESTAMP, BRANCH_A);
+  private static final GitHubData INITIAL_GITHUB_DATA_B = new GitHubData(COMMIT_HASH_B, TIMESTAMP, BRANCH_B);
 
-  private final BuildInfo BUILD_INFO_A = new BuildInfo(INITIAL_GITHUB_DATA_A);
-  private final BuildInfo BUILD_INFO_B = new BuildInfo(INITIAL_GITHUB_DATA_B);
+  private static final BuildInfo BUILD_INFO_A = new BuildInfo(INITIAL_GITHUB_DATA_A);
+  private static final BuildInfo BUILD_INFO_B = new BuildInfo(INITIAL_GITHUB_DATA_B);
 
-  private final BuildBotData BUILDER_A = new BuildBotData(
+  private static final BuildBotData BUILDER_A = new BuildBotData(
                                         COMMIT_HASH_A, "Builder A", LOGS_A, BuilderStatus.PASSED);
-  private final BuildBotData BUILDER_B = new BuildBotData(
+  private static final BuildBotData BUILDER_B = new BuildBotData(
                                          COMMIT_HASH_B, "Builder B", LOGS_B, BuilderStatus.FAILED);
 
   private final int ONE_REVISION = 1;
@@ -62,7 +61,7 @@ public class DashboardContollerTest {
   private final int OFFSET_ZERO = 0;
 
   @BeforeClass
-  public void createBuildInfos() {
+  public static void createBuildInfos() {
     BUILD_INFO_A.addBuilder(BUILDER_A);
     BUILD_INFO_B.addBuilder(BUILDER_B);
   }
@@ -89,5 +88,7 @@ public class DashboardContollerTest {
           ONE_REVISION, OFFSET_ZERO)
        .contentType("application/json")
        .accept(MediaType.APPLICATION_JSON));
+    result.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("[0].commitHash").value(COMMIT_HASH_A));
   }
 }
