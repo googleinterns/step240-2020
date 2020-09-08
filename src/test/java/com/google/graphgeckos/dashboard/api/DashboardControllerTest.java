94cc8f77ceeb5d8573d5656b7c41e1a973068e0a
holounic <mia.ilminsckaya@yandex.ru>
2020-09-08 22:17:29 +0300

    add zero revision-zero offset test

--- src/test/java/com/google/graphgeckos/dashboard/api/DashboardControllerTest.java
+++ src/test/java/com/google/graphgeckos/dashboard/api/DashboardControllerTest.java
@@ -3,40 +3,38 @@ package com.google.graphgeckos.dashboard.api;
 import com.google.cloud.Timestamp;
 import com.google.graphgeckos.dashboard.datatypes.*;
 import com.google.graphgeckos.dashboard.storage.DatastoreRepository;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = DashboardController.class)
 @AutoConfigureMockMvc
 public class DashboardControllerTest {
 
-  @Mock
+  @MockBean
   private DatastoreRepository datastoreRepository;
-
-  @InjectMocks
-  private DashboardController controller = new DashboardController();
 
   @Autowired
   private MockMvc mvc;
@@ -64,6 +62,7 @@ public class DashboardControllerTest {
   private final BuildBotData BUILDER_B = new BuildBotData(
                                          COMMIT_HASH_B, "Builder B", LOGS_B, BuilderStatus.FAILED);
 
+  private final int ZERO_REVISIONS = 0;
   private final int ONE_REVISION = 1;
   private final int TWO_REVISIONS = 2;
   private final int OFFSET_ZERO = 0;
@@ -75,23 +74,42 @@ public class DashboardControllerTest {
 
   @Before
   public void setUpRepository() {
-
     BUILD_INFO_A.addBuilder(BUILDER_A);
     BUILD_INFO_B.addBuilder(BUILDER_B);
-
-    when(
-      datastoreRepository.getLastRevisionEntries(1, 0))
-        .thenReturn(Collections.singletonList(BUILD_INFO_A));
   }
 
   @Test
-  public void whenGivenOneRevisionOffsetZeroReturnsOneBuildInfo() throws Exception {
+  public void whenGivenZeroRevisionsOffsetZeroReturnsEmptyJson() throws Exception {
+
+    given(datastoreRepository.getLastRevisionEntries(ZERO_REVISIONS, OFFSET_ZERO))
+      .willReturn(Collections.emptyList());
+
     ResultActions result = mvc.perform(
-        MockMvcRequestBuilders.get("/builders/number=1/offset=0")
-       .contentType("application/json")
-       .characterEncoding("utf-8")
+      MockMvcRequestBuilders.get("/builders/number={number}/offset={offset}",
+        ZERO_REVISIONS, OFFSET_ZERO)
+        .accept(MediaType.APPLICATION_JSON));
+
+    String emptyJsonList = "[]";
+    result.andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andExpect(content().string(emptyJsonList));
+  }
+
+  @Test
+  public void whenGivenOneRevisionOffsetZeroReturnsOneBuildInfoJson() throws Exception {
+
+    given(datastoreRepository.getLastRevisionEntries(ONE_REVISION, OFFSET_ZERO))
+      .willReturn(Collections.singletonList(BUILD_INFO_A));
+
+    ResultActions result = mvc.perform(
+        MockMvcRequestBuilders.get("/builders/number={number}/offset={offset}",
+          ONE_REVISION, OFFSET_ZERO)
        .accept(MediaType.APPLICATION_JSON));
-    result.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-          .andExpect(jsonPath("[0].commitHash").value(COMMIT_HASH_A));
+
+    result.andExpect(status().isOk())
+          .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("$[0].commitHash").value(COMMIT_HASH_A))
+          .andExpect(jsonPath("$[0].branch").value(BRANCH_A));
+
   }
 }
