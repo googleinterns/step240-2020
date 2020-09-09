2aa7ebf84099325b2d025e243ed8e2790ed17e18
holounic <mia.ilminsckaya@yandex.ru>
2020-09-09 12:42:50 +0300

    remove wildcard import

--- src/main/java/com/google/graphgeckos/dashboard/api/DashboardController.java
+++ src/main/java/com/google/graphgeckos/dashboard/api/DashboardController.java
@@ -5,7 +5,13 @@ import com.google.graphgeckos.dashboard.storage.DatastoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DashboardController {
