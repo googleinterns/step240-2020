519c8b44e58319b3696238666da68c4353020d6c
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-09 10:48:52 +0000

    Create BuilderGrid component

---
+++ src/main/webapp/components/BuilderGrid.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+
+/**
+ * Component Description
+ *
+ * @param
+ */
+export const BuilderGrid = (props) => {
+  return (
+    <div className='builderGrid'>
+      {props.builders}
+    </div>
+  );
+}
