d8a02fd7dcbd90af485c9c7566abac703461c590
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-09 13:25:18 +0000

    Add navigation options

--- src/main/webapp/components/Header.js
+++ src/main/webapp/components/Header.js
@@ -1,6 +1,17 @@
 import * as React from "react";
 import { Navigation } from "./Navigation";
 
+const OPTIONS = [
+  {
+    text: 'Docs',
+    link:  'https://github.com/googleinterns/step240-2020'
+  },
+  {
+    text: 'Buildbot',
+    link: 'http://lab.llvm.org:8011/'
+  }
+];
+
 /**
  * Main Navigation Component
  */
@@ -13,7 +24,7 @@ export const Header = () => {
         <span className='title'>{TITLE}</span>
       </div>
       <div>
-        <Navigation/>
+        <Navigation options={OPTIONS}/>
       </div>
     </header>
   );
