b9649566f484fb2209af57b4440ad4c6adcf6196
holounic <mia.ilminsckaya@yandex.ru>
2020-09-10 13:09:13 +0300

    add expected output json

--- src/test/java/com/google/graphgeckos/dashboard/buildbot/BuildBotTestInfo.java
+++ src/test/java/com/google/graphgeckos/dashboard/buildbot/BuildBotTestInfo.java
@@ -1,5 +1,20 @@
 package com.google.graphgeckos.dashboard.buildbot;
 
-public class BuildBotTestInfo {
+import com.google.graphgeckos.dashboard.AbstractJsonTestInfo;
 
+public class BuildBotTestInfo extends AbstractJsonTestInfo  {
+
+  public BuildBotTestInfo(String testName) {
+    super(testName);
+  }
+
+  @Override
+  protected String getPath() {
+    return null;
+  }
+
+  @Override
+  protected void assignExpectedValues(String[] expected) {
+
+  }
 }
