7fdfaa6de1b29a0aeb6275d59ccddedc4f48560d
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-16 14:02:28 +0000

    Add classname to container instance

--- src/main/webapp/components/BuildSnapshotContainer.js
+++ src/main/webapp/components/BuildSnapshotContainer.js
@@ -36,7 +36,7 @@ export const BuildSnapshotContainer = React.memo((props) =>
     }, []);
 
     return (
-      <div>
+      <div className='buildsnapshot-container'>
         {data.map(snapshotData => <BuildSnapshot buildData={snapshotData}/>)}
       </div>
     );
