d07a815d36f85730cb5df24b1a827ebf2892795c
holounic <mia.ilminsckaya@yandex.ru>
2020-09-17 16:08:53 +0300

    remove unused dependency

--- src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
+++ src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
@@ -1,7 +1,5 @@
 package com.google.graphgeckos.dashboard;
 
-import com.google.graphgeckos.dashboard.fetchers.buildbot.BuildBotClientPopulation;
-import com.google.graphgeckos.dashboard.fetchers.buildbot.BuildBotInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.gcp.data.datastore.repository.config.EnableDatastoreRepositories;
@@ -13,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class DashboardApplication {
 
   public static void main(String[] args) {
-
     SpringApplication.run(DashboardApplication.class, args);
   }
 
