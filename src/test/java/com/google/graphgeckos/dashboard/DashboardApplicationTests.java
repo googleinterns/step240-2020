82877e0085f523687e3c7c3f88c135ebb1224d3d
holounic <mia.ilminsckaya@yandex.ru>
2020-09-21 11:21:09 +0300

    implement DashBoardApplicationTests

--- src/test/java/com/google/graphgeckos/dashboard/DashboardApplicationTests.java
+++ src/test/java/com/google/graphgeckos/dashboard/DashboardApplicationTests.java
@@ -1,8 +1,21 @@
 package com.google.graphgeckos.dashboard;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.google.graphgeckos.dashboard.api.DashboardControllerTest;
+import com.google.graphgeckos.dashboard.github.GitHubControllerTests;
+import com.google.graphgeckos.dashboard.storage.DatastoreRepositoryTests;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-@SpringBootTest
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    DatastoreRepositoryTests.class,
+    GitHubControllerTests.class,
+    DashboardControllerTest.class,
+  })
 class DashboardApplicationTests {
-  //TODO: add tests
+
+  /* Runs all tests from the classes from the @Site.SuiteClasses list. */
+  @Test
+  public void contextLoads() {}
 }
