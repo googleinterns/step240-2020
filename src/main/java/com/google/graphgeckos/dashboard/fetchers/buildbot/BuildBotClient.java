339e4a782d4dcfe5f4cfcdd7df6e15ce143dc898
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-23 13:43:25 +0000

    Change parameters of run to only expect delay

--- src/main/java/com/google/graphgeckos/dashboard/fetchers/buildbot/BuildBotClient.java
+++ src/main/java/com/google/graphgeckos/dashboard/fetchers/buildbot/BuildBotClient.java
@@ -62,7 +62,7 @@ public class GitHubClient {
    * @param buildBot name of the BuildBot as it is in the API (e.g "clang-x86_64-debian-fast")
    * @param initialBuildId the id of the BuildBot's build from where to start fetching data
    */
-  public void run(@NonNull String buildBot, long initialBuildId, long delay) {
+  public void run(@NonNull long delay) {
 
     AtomicLong buildId = new AtomicLong(initialBuildId);
     logger.info(String.format("Builder %s: started fetching from the base url: %s",
