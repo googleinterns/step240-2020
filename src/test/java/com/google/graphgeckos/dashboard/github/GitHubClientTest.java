57e07d5a3bd8b300c93754b7ba06328829503056
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-09-24 17:06:17 +0000

    Adjust package

--- src/test/java/com/google/graphgeckos/dashboard/github/GitHubClientTest.java
+++ src/test/java/com/google/graphgeckos/dashboard/github/GitHubClientTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.graphgeckos.dashboard.buildbot;
+package com.google.graphgeckos.dashboard.github;
 
 import com.google.cloud.Timestamp;
 import com.google.graphgeckos.dashboard.datatypes.BuildBotData;
