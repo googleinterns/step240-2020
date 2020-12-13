68b1ab061b031e450943dc07c7cfc3e3792fd4ef
Krasimir Georgiev <krasimir@google.com>
2020-12-13 05:02:31 +0100

    remove WAR-style packaging artifacts
    GCP doesn't work well with .war-s and Spring Boot prefers serving static
    files from src/main/resources/static.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ gcloud config set project llvm-build-dashboard
 # build app
 mvn clean package
 # deploy app
-gcloud app deploy target/step240-2020-1.0.jar
+gcloud app deploy target/step240-0.1-SNAPSHOT.war
