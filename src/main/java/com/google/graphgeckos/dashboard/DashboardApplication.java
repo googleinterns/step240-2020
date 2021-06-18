09d15e85a4181de46266b46143879a8825fc450d
Krasimir Georgiev <krasimir@google.com>
2021-06-18 13:01:21 +0200

    simplify dashboard configuration
    With the BuildBot v2 API, we only need the list of build bot names to
    configure the dashboard.

--- src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
+++ src/main/java/com/google/graphgeckos/dashboard/DashboardApplication.java
@@ -1,11 +1,12 @@
 package com.google.graphgeckos.dashboard;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import com.google.graphgeckos.dashboard.fetchers.buildbot.BuildBotClient;
 import com.google.graphgeckos.dashboard.fetchers.github.GitHubClient;
-import com.google.graphgeckos.dashboard.reader.BuildBotSetUpData;
-import com.google.graphgeckos.dashboard.reader.BuildBotSetUpReader;
 import com.google.graphgeckos.dashboard.storage.DatastoreRepository;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
@@ -17,6 +18,7 @@ import org.springframework.cloud.gcp.data.datastore.repository.config.EnableData
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -32,6 +34,14 @@ public class DashboardApplication implements SchedulingConfigurer {
 
   private static final String GITHUB_URL = "https://api.github.com/repos/llvm/llvm-project/commits";
 
+  /**
+   * Path to the dashboard configuration file, relative to src/main/resources.
+   *
+   * <p>The configuration file is an UTF-8 encoded text file. It lists the names of the build bots
+   * to load, one per line.
+   */
+  private static final String CONFIG_PATH = "static/buildbots.txt";
+
   private static final Logger logger = Logger.getLogger(DashboardApplication.class.getName());
 
   @Autowired private DatastoreRepository datastoreRepository;
@@ -40,18 +50,44 @@ public class DashboardApplication implements SchedulingConfigurer {
     SpringApplication.run(DashboardApplication.class, args);
   }
 
-  /** Periodically fetches build bot status every 2 minutes. */
-  @Scheduled(fixedDelay = 2 * 60 * 1000)
+  /**
+   * Reads the build bot names from the configuration.
+   *
+   * @param configuration dashboard configuration, listing the names of the build bots to load, one
+   *     per line.
+   */
+  static String[] readBuildBotNames(String configuration) {
+    return configuration.split("\\n");
+  }
+
+  /** 
+   * Periodically fetches build bot status every 10 minutes.
+   *
+   *
+   * @throws IllegalStateException if the configuration at CONFIG_PATH cannot be read or is empty.
+   */
+  @Scheduled(fixedDelay = 10 * 60 * 1000)
   void runBuildBotsFetchers() {
     logger.info("Fetching latest build bots status...");
-    List<BuildBotSetUpData> buildBots = new BuildBotSetUpReader().read();
-    if (buildBots == null) {
-      throw new IllegalArgumentException(
-          "List of build bots to run can't be null. Check the config file.");
+    String configuration;
+    try {
+      configuration =
+          CharStreams.toString(
+              new InputStreamReader(
+                  new ClassPathResource(CONFIG_PATH).getInputStream(), Charsets.UTF_8));
+    } catch (IOException e) {
+      throw new IllegalStateException("Cannot read configuration from: " + CONFIG_PATH, e);
     }
-    for (BuildBotSetUpData buildBot : buildBots) {
-      logger.info("Fetching builds from build bot: " + buildBot.name);
-      new BuildBotClient(BASE_URL, datastoreRepository).run(buildBot.name);
+
+    String[] buildBots = readBuildBotNames(configuration);
+    if (buildBots.length == 0) {
+      throw new IllegalStateException(
+          "List of build bots to run can't be empty. Check the config file: " + CONFIG_PATH);
+    }
+
+    for (String buildBot : buildBots) {
+      logger.info("Fetching builds from build bot: " + buildBot);
+      new BuildBotClient(BASE_URL, datastoreRepository).run(buildBot);
     }
   }
 
