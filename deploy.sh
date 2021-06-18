b7300dde8848336457c13d692cc84cae6820f61a
Krasimir Georgiev <krasimir@google.com>
2021-06-18 11:36:43 +0200

    deploy.sh: update to point to the correct archive
    The project uses .jar instead of .war.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ gcloud config set project llvm-build-dashboard
 # build app
 mvn clean package
 # deploy app
-gcloud app deploy target/step240-0.1-SNAPSHOT.war
+gcloud app deploy target/step240-0.1-SNAPSHOT.jar
